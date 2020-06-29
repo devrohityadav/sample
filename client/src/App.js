c7b0f928b00865d8616cdf14c0fbe50b132da284
Rohit yadav <dev.yadav.rohit@gmail.com>
2020-06-29 20:41:23 +0530

    Client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <form
+      method="post"
+      action="http://localhost:8080/process"
+      enctype="multipart/form-data"
+    >
+      <p>Upload Photo</p>
+      <input type="file" name="img" />
+      <input type="submit" name="Submit" />
+    </form>
+  );
 }
 
 export default App;
